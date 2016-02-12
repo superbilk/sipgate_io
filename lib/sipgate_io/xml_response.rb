ab80e817c95a4a413ad95f7d66b22e76fadb18eb
Chris Schmidt <chris@superbilk.org>
2016-02-12 21:04:19 +0100

    test coverage for XmlResponse

--- lib/sipgate_io/xml_response.rb
+++ lib/sipgate_io/xml_response.rb
@@ -3,76 +3,61 @@ require 'builder'
 module SipgateIo
   class XmlResponse
 
-    def self.dial(target, options = nil)
-      self.builder.Response do |b|
-        if target == :voicemail
-          b.Dial { |b| b.Voicemail }
-        elsif options.nil?
-          b.Dial { |b| b.Number(target) }
-        elsif !options[:anonymous].nil?
-          b.Dial(anonymous: true) { |b| b.Number(target) }
-        elsif !options[:caller_id].nil?
-          b.Dial(callerId: options[:caller_id]) { |b| b.Number(target) }
+    def self.dial(options = {})
+      anonymous = options[:clip] == :anonymous ? Hash[ anonymous: true ] : nil
+      caller_id = !!/\d+/.match(options[:clip]) ? Hash[ callerId: options[:clip] ] : nil
+      self.builder.Response(set_callback(options)) do |b|
+        b.Dial(anonymous, caller_id) do |b|
+          options[:target] == :voicemail ? b.Voicemail : b.Number(options[:target])
         end
       end
     end
 
-    def self.play(url)
-      self.builder.Response { |b| b.Play { |b| b.Url(url) } }
+    def self.play(options = {})
+      url = options[:soundfile_url]
+      self.builder.Response(set_callback(options)) { |b| b.Play { |b| b.Url(url) } }
     end
 
-    def self.gather(options = nil)
-      options ||= {}
-      options[:type] ||= :dtmf
-      options[:callback_url] ||= SipgateIo.configuration.callback_url
-      options[:max_digits] ||= 1
-      options[:timeout] ||= 1000
-      self.builder.Response do |b|
-        b.Gather(onData: options[:callback_url],
-                 maxDigits: options[:max_digits],
-                 timeout: options[:timeout]) do |b|
-          unless options[:play_url].nil?
-            b.Play { |b| b.Url(options[:play_url]) }
+    def self.gather(options = {})
+      on_data = Hash[ onData: SipgateIo.configuration.callback_url ]
+      max_digits = options.key?(:max_digits) ? Hash[ maxDigits: options[:max_digits] ] : nil
+      timeout = options.key?(:timeout) ? Hash[ timeout: options[:timeout] ] : nil
+      play_url = options.key?(:soundfile_url) ? options[:soundfile_url] : nil
+
+      self.builder.Response(set_callback(options)) do |b|
+        b.Gather(on_data,
+                 max_digits,
+                 timeout) do |b|
+          unless play_url.nil?
+            b.Play { |b| b.Url(play_url) }
           end
         end
       end
     end
 
-    def self.reject(reason = nil)
-      self.builder.Response do |b|
-        if reason.nil?
-          b.Reject
-        else
-          b.Reject(reason: reason)
-        end
-      end
+    def self.reject(options = {})
+      reason = options.key?(:reason) ? Hash[ reason: options[:reason] ] : nil
+      self.builder.Response(set_callback(options)) { |b| b.Reject(reason) }
     end
 
-    def self.hangup(callback = nil)
-      validate_callback! callback
-
-      self.builder.Response(callback){ |b| b.Hangup }
+    def self.hangup(options = {})
+      self.builder.Response(set_callback(options)){ |b| b.Hangup }
     end
 
     def self.on_answer
-      self.builder.Response(onAnswer: SipgateIo.configuration.callback_url)
+      self.builder.Response(set_callback(Hash[ callback: :on_answer ]) )
     end
 
     def self.on_hangup
-      self.builder.Response(onHangup: SipgateIo.configuration.callback_url)
+      self.builder.Response(set_callback(Hash[ callback: :on_hangup ]) )
     end
 
     private
 
-    def self.validate_callback!(callback)
-      unless callback.nil?
-        callback.keys.each do |k|
-          callback.delete k unless [:on_answer, :on_hangup].include? k
-        end
-        callback[:onAnswer] = callback.delete :on_answer if callback[:on_answer]
-        callback[:onHangup] = callback.delete :on_hangup if callback[:on_hangup]
-      end
-      callback
+    def self.set_callback(options)
+      return nil unless options.key?(:callback)
+      type = (options[:callback] == :on_answer) ? :onAnswer : :onHangup
+      Hash[ type => SipgateIo.configuration.callback_url ]
     end
 
     def self.builder
@@ -83,17 +68,3 @@ module SipgateIo
 
   end
 end
-
-# puts SipgateIo::XmlResponse.gather()
-# puts SipgateIo::XmlResponse.gather(play_url: "http://google.de")
-
-# puts SipgateIo::XmlResponse.play("http://google.de")
-# puts SipgateIo::XmlResponse.dial(:voicemail)
-# puts SipgateIo::XmlResponse.dial("491234567")
-# puts SipgateIo::XmlResponse.dial("491234567", anonymous: true)
-# puts SipgateIo::XmlResponse.dial("491234567", caller_id: "555")
-
-# puts SipgateIo::XmlResponse.reject
-# puts SipgateIo::XmlResponse.reject(:busy)
-# puts SipgateIo::XmlResponse.reject(:rejected)
-# puts SipgateIo::XmlResponse.hangup
--- test/lib/sipgate_io/xml_response_test.rb
+++ test/lib/sipgate_io/xml_response_test.rb
@@ -6,28 +6,134 @@ module SipgateIo
     test "hangup xml response without parameter" do
       actual = SipgateIo::XmlResponse.hangup
       expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Hangup/></Response>]
-
       assert_equal sanitize_xml_for_test(actual), expected
     end
 
     test "hangup xml response with on_answer parameter" do
-      actual = SipgateIo::XmlResponse.hangup(on_answer: "http://google.de")
-      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onAnswer="http://google.de"><Hangup/></Response>]
-
+      actual = SipgateIo::XmlResponse.hangup(callback: :on_answer)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onAnswer="http://localhost:3000"><Hangup/></Response>]
       assert_equal sanitize_xml_for_test(actual), expected
     end
 
     test "hangup xml response with on_hangup parameter" do
-      actual = SipgateIo::XmlResponse.hangup(on_hangup: "http://google.de")
-      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onHangup="http://google.de"><Hangup/></Response>]
-
+      actual = SipgateIo::XmlResponse.hangup(callback: :on_hangup)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onHangup="http://localhost:3000"><Hangup/></Response>]
       assert_equal sanitize_xml_for_test(actual), expected
     end
 
-    test "hangup xml response with invalid parameter" do
-      actual = SipgateIo::XmlResponse.hangup(foo: "bar")
-      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Hangup/></Response>]
+    test "reject without reason" do
+      actual = SipgateIo::XmlResponse.reject
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Reject/></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
 
+    test "reject with reason" do
+      actual = SipgateIo::XmlResponse.reject(reason: :busy)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Reject reason="busy"/></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+
+      actual = SipgateIo::XmlResponse.reject(reason: :rejected)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Reject reason="rejected"/></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "reject with reason and callback" do
+      actual = SipgateIo::XmlResponse.reject(reason: :busy, callback: :on_hangup)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onHangup="http://localhost:3000"><Reject reason="busy"/></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+
+      actual = SipgateIo::XmlResponse.reject(reason: :rejected, callback: :on_answer)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onAnswer="http://localhost:3000"><Reject reason="rejected"/></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "play with url" do
+      actual = SipgateIo::XmlResponse.play(soundfile_url: "http://file.wav")
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Play><Url>http://file.wav</Url></Play></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "play with url and callback" do
+      actual = SipgateIo::XmlResponse.play(soundfile_url: "http://file.wav", callback: :on_hangup)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onHangup="http://localhost:3000"><Play><Url>http://file.wav</Url></Play></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "gather xml response without parameter" do
+      actual = SipgateIo::XmlResponse.gather
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Gather onData="http://localhost:3000"></Gather></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+
+      actual = SipgateIo::XmlResponse.gather()
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Gather onData="http://localhost:3000"></Gather></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "gather xml response with callback" do
+      actual = SipgateIo::XmlResponse.gather(callback: :on_hangup)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onHangup="http://localhost:3000"><Gather onData="http://localhost:3000"></Gather></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "gather xml response with parameter" do
+      actual = SipgateIo::XmlResponse.gather(soundfile_url: "http://file.wav")
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Gather onData="http://localhost:3000"><Play><Url>http://file.wav</Url></Play></Gather></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+
+      actual = SipgateIo::XmlResponse.gather(soundfile_url: "http://file.wav", timeout: 5000)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Gather onData="http://localhost:3000" timeout="5000"><Play><Url>http://file.wav</Url></Play></Gather></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "gather xml response with parameter and callback" do
+      actual = SipgateIo::XmlResponse.gather(soundfile_url: "http://file.wav", callback: :on_hangup)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onHangup="http://localhost:3000"><Gather onData="http://localhost:3000"><Play><Url>http://file.wav</Url></Play></Gather></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+
+      actual = SipgateIo::XmlResponse.gather(soundfile_url: "http://file.wav", max_digits: 5, callback: :on_answer)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onAnswer="http://localhost:3000"><Gather onData="http://localhost:3000" maxDigits="5"><Play><Url>http://file.wav</Url></Play></Gather></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "call redirect to voicemail" do
+      actual = SipgateIo::XmlResponse.dial(target: :voicemail)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Dial><Voicemail/></Dial></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "call redirect to number" do
+      actual = SipgateIo::XmlResponse.dial(target: "4915799912345")
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Dial><Number>4915799912345</Number></Dial></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "call redirect to number with anonymous" do
+      actual = SipgateIo::XmlResponse.dial(target: "4915799912345", clip: :anonymous)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Dial anonymous="true"><Number>4915799912345</Number></Dial></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "call redirect to number with clip" do
+      actual = SipgateIo::XmlResponse.dial(target: "4915799912345", clip: "4915799912345")
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response><Dial callerId="4915799912345"><Number>4915799912345</Number></Dial></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "call redirect to number with clip and callback" do
+      actual = SipgateIo::XmlResponse.dial(target: "4915799912345", clip: "4915799912345", callback: :on_answer)
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onAnswer="http://localhost:3000"><Dial callerId="4915799912345"><Number>4915799912345</Number></Dial></Response>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "only on answer" do
+      actual = SipgateIo::XmlResponse.on_answer
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onAnswer="http://localhost:3000"/>]
+      assert_equal sanitize_xml_for_test(actual), expected
+    end
+
+    test "only on hangup" do
+      actual = SipgateIo::XmlResponse.on_hangup
+      expected = %Q[<?xml version="1.0" encoding="UTF-8"?><Response onHangup="http://localhost:3000"/>]
       assert_equal sanitize_xml_for_test(actual), expected
     end
 
