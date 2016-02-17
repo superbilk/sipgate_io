55d5e5c614116e0859f45f87dc9fb208cc1babba
Chris Schmidt <chris@superbilk.org>
2016-02-17 19:24:30 +0100

    Add custom response header

--- app/controllers/sipgate_io/events_controller.rb
+++ app/controllers/sipgate_io/events_controller.rb
@@ -3,6 +3,7 @@ require_dependency "sipgate_io/application_controller"
 module SipgateIo
   class EventsController < ApplicationController
     skip_before_action :verify_authenticity_token, raise: false
+    before_action :add_response_header
 
     def create
       (head 500 and return) unless params.key?(:event)
@@ -17,6 +18,10 @@ module SipgateIo
     end
 
     private
+
+    def add_response_header
+      response.headers["X-API-Client"] = "sipgate_io-#{SipgateIo::VERSION} (source: https://github.com/superbilk/sipgate_io)"
+    end
 
     def create_event(params)
       event_type = params[:event].clone
--- lib/sipgate_io/version.rb
+++ lib/sipgate_io/version.rb
@@ -1,3 +1,3 @@
 module SipgateIo
-  VERSION = "0.3.1"
+  VERSION = "0.3.2"
 end
